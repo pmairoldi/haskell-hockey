f2fea302a002cb36be60021c587b44ab890564db
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-03-29 00:05:06 -0400

    More json parsing

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,11 @@ import Data.List as List
 import Data.Aeson
 import Control.Applicative as Applicative
 import GHC.Generics
+import Data.UTC
+import Data.Maybe
+import Data.String
+import Data.List.Split as Split
+
 -- import Data.Typeable
 -- import Data.Data
 -- import Control.Monad.IO.Class
@@ -14,7 +19,7 @@ import qualified Data.ByteString.Lazy as LazyByteString
 import qualified Data.ByteString.Char8 as ByteString
 
 -- Load from environment variables
-currentYear :: Int
+currentYear :: Integer
 currentYear = 2014
 
 currentSeason :: Season
@@ -24,26 +29,72 @@ data JSONType = JSON | JSONP | HTML deriving (Enum, Show, Eq)
 
 data Season = Preseason | Season | Playoffs deriving (Enum, Show, Eq)
 
-fromSeason :: Season -> Int
+fromSeason :: Season -> Integer
 fromSeason Preseason = 1
 fromSeason Season = 2
 fromSeason Playoffs = 3
 
-toSeason :: Int -> Season
+toSeason :: Integer -> Season
 toSeason 1 = Preseason
 toSeason 2 = Season
 toSeason 3 = Playoffs
 
+-- Formatters
+digitFormat :: Int -> Integer -> String
+digitFormat digits number = LazyText.unpack (format (left digits '0') number)
+
+formattedGame :: Integer -> String
+formattedGame game = digitFormat 4 game
+
+formattedSeason :: Season -> String
+formattedSeason season = digitFormat 2 (fromSeason season)
+
+formattedMonth :: Integer -> String
+formattedMonth month = digitFormat 2 month
+
+formattedDay :: Integer -> String
+formattedDay day = digitFormat 2 day
+
+formattedYear :: Integer -> String
+formattedYear year = LazyText.unpack (format (int) year)
+
+fullGameId :: Integer -> Season -> Integer -> String
+fullGameId year season game = (formattedYear year) ++ (formattedSeason season) ++ (formattedGame game)
+
+fullYear :: Integer -> String
+fullYear year = (formattedYear year) ++ (formattedYear (year + 1))
+
+fullDate :: Integer -> Integer -> Integer -> String
+fullDate year month day = (formattedYear year) ++ "-" ++ (formattedMonth month) ++ "-" ++ (formattedDay day)
+
+unpackToLower :: Text -> String
+unpackToLower v = LazyText.unpack (LazyText.toLower v)
+
+dateFromComponents :: Integer -> Integer -> Integer -> Date
+dateFromComponents year month day = fromJust $ setYear year (epoch :: Date) >>= setMonth month >>= setDay day
+
+stringToInteger :: String -> Integer
+stringToInteger s = read s :: Integer
+
+dateStringToComponents :: Text -> [Integer]
+dateStringToComponents text = List.map stringToInteger $ Split.splitOn "/" $ LazyText.unpack text
+
+unpackParseDate :: Text -> Date
+unpackParseDate text =
+    let components = dateStringToComponents text
+    in dateFromComponents (components !! 2) (components !! 0)  (components !! 1)
+
+-- Request helpers
 openUrl :: String -> IO String
 openUrl url = simpleHTTP (getRequest url) >>= getResponseBody
 
 jsonpRequestParse :: IO String -> IO String
-jsonpRequestParse response = do 
+jsonpRequestParse response = do
     body <- response
     return $ jsonpToJson body
 
 requestParse :: IO String -> IO String
-requestParse response = do 
+requestParse response = do
     body <- response
     return $ body
 
@@ -57,135 +108,138 @@ dropAndReverse :: Eq a => a -> [a] -> [a]
 dropAndReverse delimiter content = List.reverse (List.dropWhile (/= delimiter) content)
 
 jsonpToJson :: String -> String
-jsonpToJson jsonp 
+jsonpToJson jsonp
     | and $ [("{" `List.isPrefixOf` jsonp), ("}" `List.isSuffixOf` jsonp)] = jsonp
     | otherwise = dropAndReverse '}' (dropAndReverse '{' jsonp)
 
 -- Urls
-playByPlayUrl :: Int -> Season -> Int -> String
+playByPlayUrl :: Integer -> Season -> Integer -> String
 playByPlayUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/PlayByPlay.json"
 
-summaryUrl :: Int -> Season -> Int -> String
+summaryUrl :: Integer -> Season -> Integer -> String
 summaryUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/Summary.json"
 
-eventVideoUrl :: Int -> Season -> Int -> String
+eventVideoUrl :: Integer -> Season -> Integer -> String
 eventVideoUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/gc/gcgm.jsonp"
 
-videoUrl :: Int -> Season -> Int -> String
+videoUrl :: Integer -> Season -> Integer -> String
 videoUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/gc/gcsb.jsonp"
 
-boxscoreUrl :: Int -> Season -> Int -> String
+boxscoreUrl :: Integer -> Season -> Integer -> String
 boxscoreUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/gc/gcbx.jsonp"
 
-gameSummaryUrl :: Int -> Season -> Int -> String
+gameSummaryUrl :: Integer -> Season -> Integer -> String
 gameSummaryUrl year season game = "http://live.nhl.com/GameData/" ++ (fullYear year) ++ "/" ++ (fullGameId year season game) ++ "/gc/gcgs.jsonp"
 
-resultsUrl :: Int -> Int -> Int -> String
+resultsUrl :: Integer -> Integer -> Integer -> String
 resultsUrl year month day = "http://live.nhl.com/GameData/GCScoreboard/" ++ (fullDate year month day) ++ ".jsonp"
 
-boxscoreHTMLUrl :: Int -> Season -> Int -> String
+boxscoreHTMLUrl :: Integer -> Season -> Integer -> String
 boxscoreHTMLUrl year season game = "http://www.nhl.com/gamecenter/en/boxscore?id=" ++ (fullGameId year season game)
 
--- Formatters
-digitFormat :: Int -> Int -> String 
-digitFormat digits number = LazyText.unpack (format (left digits '0') number)
-
-formattedGame :: Int -> String
-formattedGame game = digitFormat 4 game
-
-formattedSeason :: Season -> String
-formattedSeason season = digitFormat 2 (fromSeason season)
-
-formattedMonth :: Int -> String
-formattedMonth month = digitFormat 2 month
-
-formattedDay :: Int -> String
-formattedDay day = digitFormat 2 day
-
-formattedYear :: Int -> String
-formattedYear year = LazyText.unpack (format (int) year)
-
-fullGameId :: Int -> Season -> Int -> String
-fullGameId year season game = (formattedYear year) ++ (formattedSeason season) ++ (formattedGame game)
-
-fullYear :: Int -> String
-fullYear year = (formattedYear year) ++ (formattedYear (year + 1))
-
-fullDate :: Int -> Int -> Int -> String
-fullDate year month day = (formattedYear year) ++ "-" ++ (formattedMonth month) ++ "-" ++ (formattedDay day)
-
 -- HTTP requests
-getPlayByPlayResponse :: Int -> Season -> Int -> IO String
+getPlayByPlayResponse :: Integer -> Season -> Integer -> IO String
 getPlayByPlayResponse year season game = get (playByPlayUrl year season game) JSON
 
-getSummaryResponse :: Int -> Season -> Int -> IO String
+getSummaryResponse :: Integer -> Season -> Integer -> IO String
 getSummaryResponse year season game = get (summaryUrl year season game) JSON
 
-getEventVideosResponse :: Int -> Season -> Int -> IO String
+getEventVideosResponse :: Integer -> Season -> Integer -> IO String
 getEventVideosResponse year season game = get (eventVideoUrl year season game) JSONP
 
-getVideosResponse :: Int -> Season -> Int -> IO String
+getVideosResponse :: Integer -> Season -> Integer -> IO String
 getVideosResponse year season game = get (videoUrl year season game) JSONP
 
-getBoxscoreResponse :: Int -> Season -> Int -> IO String
+getBoxscoreResponse :: Integer -> Season -> Integer -> IO String
 getBoxscoreResponse year season game = get (boxscoreUrl year season game) JSONP
 
-getGameSummaryResponse :: Int -> Season -> Int -> IO String
+getGameSummaryResponse :: Integer -> Season -> Integer -> IO String
 getGameSummaryResponse year season game = get (gameSummaryUrl year season game) JSONP
 
-getResultsResponse :: Int -> Int -> Int -> IO String
+getResultsResponse :: Integer -> Integer -> Integer -> IO String
 getResultsResponse year month day = get (resultsUrl year month day) JSONP
 
 -- Parse JSON
 
-data Game = Game { awayId :: String, homeId :: String } deriving (Show, Generic)
+data GameState = None | Before | Ongoing | Overtime | Final deriving (Enum, Show, Eq, Generic)
+
+fromGameState :: GameState -> Integer
+fromGameState None = 1
+fromGameState Before = 2
+fromGameState Ongoing = 3
+fromGameState Overtime = 4
+fromGameState Final = 4
+
+toGameState :: Integer -> GameState
+toGameState 1 = None
+toGameState 2 = Before
+toGameState 3 = Ongoing
+toGameState 4 = Overtime
+toGameState 5 = Final
+
+instance FromJSON GameState
+
+data Game = Game { id :: Integer, awayId :: String, homeId :: String, caTV :: String, usTV :: String, gameState :: GameState } deriving (Show, Generic)
 
 parseGame v = Game <$>
-    v .: "ata" <*>
-    v .: "hta"
+    v .: "id" <*>
+    fmap unpackToLower (v .: "ata") <*>
+    fmap unpackToLower (v .: "hta") <*>
+    v .: "canationalbroadcasts" <*>
+    v .: "usnationalbroadcasts" <*>
+    fmap toGameState (v .:"gs")
 
 instance FromJSON Game where
-  parseJSON (Object v) = parseGame v
-  parseJSON _          = Applicative.empty
+    parseJSON (Object v) = parseGame v
+    parseJSON _          = Applicative.empty
 
-data Results = Results { games :: [Game] } deriving (Show, Generic)
-instance FromJSON Results
+data Results = Results { games :: [Game], currentDate :: Date, nextDate :: Date, prevDate :: Date } deriving (Show, Generic)
+
+parseResults v = Results <$>
+    v .: "games" <*>
+    fmap unpackParseDate (v .: "currentDate") <*>
+    fmap unpackParseDate (v .: "nextDate") <*>
+    fmap unpackParseDate (v .: "prevDate")
+
+instance FromJSON Results where
+    parseJSON (Object v) = parseResults v
+    parseJSON _          = Applicative.empty
 
 stringToLazyByteString :: String -> LazyByteString.ByteString
 stringToLazyByteString string = LazyByteString.fromStrict (ByteString.pack string)
 
 decodeResponse :: (FromJSON a) => IO String -> IO (Maybe a)
-decodeResponse response = do 
+decodeResponse response = do
     rsp <- response
     return $ decode (stringToLazyByteString rsp)
 
 -- Start add concrete types
 
-getPlayByPlay :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getPlayByPlay :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getPlayByPlay year season game = decodeResponse $ getPlayByPlayResponse year season game
 
-getSummary :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getSummary :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getSummary year season game = decodeResponse $ getSummaryResponse year season game
 
-getEventVideos :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getEventVideos :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getEventVideos year season game = decodeResponse $ getEventVideosResponse year season game
 
-getVideos :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getVideos :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getVideos year season game = decodeResponse $ getVideosResponse year season game
 
-getBoxscore :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getBoxscore :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getBoxscore year season game = decodeResponse $ getBoxscoreResponse year season game
 
-getGameSummary :: (FromJSON a) => Int -> Season -> Int -> IO (Maybe a)
+getGameSummary :: (FromJSON a) => Integer -> Season -> Integer -> IO (Maybe a)
 getGameSummary year season game = decodeResponse $ getGameSummaryResponse year season game
 
 -- End add concrete types
 
-getResults :: Int -> Int -> Int -> IO (Maybe Results)
-getResults year month day = decodeResponse $ getResultsResponse year month day
+getResults :: Date -> IO (Maybe Results)
+getResults date = decodeResponse $ getResultsResponse (year date) (month date) (day date)
 
 main = print "hello world"
 -- main :: IO()
--- main = do 
---     src <- getGameSummaryResponse 2014 Season 111 
---     print src
+-- main = do
+--     src <- getGameSummaryResponse 2014 Season 111
+--     prInteger src
