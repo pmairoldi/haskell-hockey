6ab25f23249fece085b81fc1ecdc2ab0f79be22c
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2022-05-02 22:00:17 -0400

    fix upsert

--- src/Hockey/Database.hs
+++ src/Hockey/Database.hs
@@ -17,11 +17,20 @@ import Hockey.Database.Types
 import Database.Persist
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
+import Control.Monad (liftM)
+
+insertOrReplace :: (MonadIO m, PersistUniqueWrite backend, OnlyOneUniqueKey record, PersistEntityBackend record ~ BaseBackend backend) => record -> ReaderT backend m ()
+insertOrReplace record = do
+    uniqueKey <- onlyUnique record
+    mExists <- getBy uniqueKey
+    case mExists of
+        Just (Entity k _) -> replace k record
+        Nothing           -> insert record >> return ()
 
 upsertMany :: (MonadIO m, PersistUniqueWrite backend, OnlyOneUniqueKey record, PersistEntityBackend record ~ BaseBackend backend) => [record] -> ReaderT backend m ()
 upsertMany [] = return ()
 upsertMany (x:xs) = do
-    upsert x []
+    insertOrReplace x
     upsertMany xs
 
 insertManyUnique :: (MonadIO m, PersistUniqueWrite backend, PersistEntity record, PersistEntityBackend record ~ BaseBackend backend) => [record] -> ReaderT backend m ()
